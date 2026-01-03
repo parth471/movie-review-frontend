ce31e623e4e4868fd8ea7ac9dc91f3cf2da77d44
parth471 <parth1042020@gmail.com>
2026-01-03 22:49:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import MovieDetails from "./pages/MovieDetails";
+import movieDetails from "./pages/movieDetails";
 
 function App() {
   return (
