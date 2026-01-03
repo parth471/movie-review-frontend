8ca9aaa82ae8cbc4dfe1e3d57b60c4949b93648c
parth471 <parth1042020@gmail.com>
2026-01-03 22:41:50 +0530

    Update movieService.js

--- src/api/movieService.js
+++ src/api/movieService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/v1/movies";
+const API_URL = "https://movie-review-backend-1r30.onrender.com/api/v1/movies";
 
 export const getAllMovies = async () => {
   const response = await axios.get(API_URL);
