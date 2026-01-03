d2dcfff024a2d35312b90011061b8535b06d23dc
parth471 <parth1042020@gmail.com>
2026-01-03 22:42:11 +0530

    Update reviewService.js

--- src/api/reviewService.js
+++ src/api/reviewService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REVIEW_API = "http://localhost:8080/api/v1/reviews";
+const REVIEW_API = "https://movie-review-backend-1r30.onrender.com/api/v1/reviews";
 
 export const addReview = async (reviewBody, imdbId) => {
   const response = await axios.post(REVIEW_API, {
