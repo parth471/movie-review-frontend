745e380133d77c4aec4a7fb5189aaeaff3756c00
Parth Patel <parth1042020@gmail.com>
2026-01-04 18:16:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/movieDetails";
+
+/* Scroll to top on route change (mobile-friendly) */
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:imdbId" element={<MovieDetails />} />
